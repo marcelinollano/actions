79f799e8e0c5da32702ea610c3b54cabef357e30
Marcelino Llano <marcelinollano@users.noreply.github.com>
2020-12-29 21:10:14 +0100

    Update entrypoint.sh

--- dokku/entrypoint.sh
+++ dokku/entrypoint.sh
@@ -3,10 +3,8 @@
 set -e
 
 SSH_PATH="$HOME/.ssh"
-
 mkdir -p "$SSH_PATH"
 touch "$SSH_PATH/known_hosts"
-
 echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
 
@@ -18,6 +16,6 @@ chmod 600 "$SSH_PATH/deploy_key.pub"
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/deploy_key"
 ssh-keyscan -t rsa $DOKKU_HOST >> "$SSH_PATH/known_hosts"
-git checkout $BRANCH_FROM
 
+git checkout $BRANCH_FROM
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push ssh://$DOKKU_USER@$DOKKU_HOST:$DOKKU_PORT/$DOKKU_APP $BRANCH_FROM:$BRANCH_TO
